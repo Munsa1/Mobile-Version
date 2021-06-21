0e08204f4a3114be454a8f41b19c58bb1593ec0a
Tolibjon <tolibjohn@gmail.com>
2021-06-21 19:38:38 +0500

    Add styling for mobile menu

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ menu.className = "menu-style";
 body.appendChild(menu);
 
 let icon = document.createElement("i");
-icon.className = "fas fa-times";
+icon.className = "fa fa-times";
 menu.appendChild(icon);
 
 let menuList = document.createElement("ul");
@@ -14,20 +14,26 @@ menu.appendChild(menuList);
 let portfolio = document.createElement("li");
 portfolio.className = "portfolio";
 let portfolioLink = document.createElement("a");
+portfolioLink.className="mobile-a";
 portfolioLink.textContent = "Portfolio";
 portfolioLink.href = "#portfolio";
 portfolio.appendChild(portfolioLink);
 menuList.appendChild(portfolio);
 
 let about = document.createElement("li");
-about.className = "about";
+about.className = "mobile-about";
 let aboutLink = document.createElement("a");
+aboutLink.className="mobile-a";
 aboutLink.textContent = "About";
 aboutLink.href = "#about";
+about.appendChild(aboutLink);
+menuList.appendChild(about);
+
 let contact = document.createElement("li");
 contact.className = "contatc-content";
 let contactLink = document.createElement("a");
 contactLink.textContent = "Contact";
+contactLink.className="mobile-a";
 contactLink.href = "#contact";
 contact.appendChild(contactLink);
 menuList.appendChild(contact);
