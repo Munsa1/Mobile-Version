73221150e8eb4e2e4958124fdce269c78831a63a
PolinaStamenova <polina.stamenova@gmail.com>
2021-06-21 18:03:06 +0300

    Add click event to top-section

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ body.appendChild(menu);
 
 let icon = document.createElement("i");
 icon.className = "fa fa-times";
+icon.id = "x-close";
 menu.appendChild(icon);
 
 let menuList = document.createElement("ul");
@@ -14,18 +15,18 @@ menu.appendChild(menuList);
 let portfolio = document.createElement("li");
 portfolio.className = "portfolio";
 let portfolioLink = document.createElement("a");
-portfolioLink.className="mobile-a";
+portfolioLink.className = "mobile-a";
 portfolioLink.textContent = "Portfolio";
-portfolioLink.href = "#portfolio";
+portfolioLink.href = "#portfolio-section";
 portfolio.appendChild(portfolioLink);
 menuList.appendChild(portfolio);
 
 let about = document.createElement("li");
 about.className = "mobile-about";
 let aboutLink = document.createElement("a");
-aboutLink.className="mobile-a";
+aboutLink.className = "mobile-a";
 aboutLink.textContent = "About";
-aboutLink.href = "#about";
+aboutLink.href = "#about-section";
 about.appendChild(aboutLink);
 menuList.appendChild(about);
 
@@ -33,7 +34,29 @@ let contact = document.createElement("li");
 contact.className = "contatc-content";
 let contactLink = document.createElement("a");
 contactLink.textContent = "Contact";
-contactLink.className="mobile-a";
-contactLink.href = "#contact";
+contactLink.className = "mobile-a";
+contactLink.href = "#contact-section";
 contact.appendChild(contactLink);
 menuList.appendChild(contact);
+
+let bars = document.getElementById("bars");
+bars.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+
+let close = document.getElementById("x-close");
+close.addEventListener("click", () => {
+  menu.style.display = "none";
+});
+
+document.getElementsByClassName("mobile-a")[0].addEventListener("click", () => {
+  menu.style.display = "none";
+});
+
+document.getElementsByClassName("mobile-a")[1].addEventListener("click", () => {
+  menu.style.display = "none";
+});
+
+document.getElementsByClassName("mobile-a")[2].addEventListener("click", () => {
+  menu.style.display = "none";
+});
