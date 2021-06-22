bc68aaca1768a20c63d6de4fa8ecf8628fc47c25
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-06-22 16:29:08 +0200

    Finalize mobile view and add functionality

--- javascript.js
+++ javascript.js
@@ -1,62 +1,62 @@
-let body = document.getElementById("body-project");
-let menu = document.createElement("div");
-menu.className = "menu-style";
+const body = document.getElementById('body-project');
+const menu = document.createElement('div');
+menu.className = 'menu-style';
 body.appendChild(menu);
 
-let icon = document.createElement("i");
-icon.className = "fa fa-times";
-icon.id = "x-close";
+const icon = document.createElement('i');
+icon.className = 'fa fa-times';
+icon.id = 'x-close';
 menu.appendChild(icon);
 
-let menuList = document.createElement("ul");
-menuList.className = "menu-list";
+const menuList = document.createElement('ul');
+menuList.className = 'menu-list';
 menu.appendChild(menuList);
 
-let portfolio = document.createElement("li");
-portfolio.className = "portfolio";
-let portfolioLink = document.createElement("a");
-portfolioLink.className = "mobile-a";
-portfolioLink.textContent = "Portfolio";
-portfolioLink.href = "#portfolio-section";
+const portfolio = document.createElement('li');
+portfolio.className = 'portfolio';
+const portfolioLink = document.createElement('a');
+portfolioLink.className = 'mobile-a';
+portfolioLink.textContent = 'Portfolio';
+portfolioLink.href = '#portfolio-section';
 portfolio.appendChild(portfolioLink);
 menuList.appendChild(portfolio);
 
-let about = document.createElement("li");
-about.className = "mobile-about";
-let aboutLink = document.createElement("a");
-aboutLink.className = "mobile-a";
-aboutLink.textContent = "About";
-aboutLink.href = "#about-section";
+const about = document.createElement('li');
+about.className = 'mobile-about';
+const aboutLink = document.createElement('a');
+aboutLink.className = 'mobile-a';
+aboutLink.textContent = 'About';
+aboutLink.href = '#about-section';
 about.appendChild(aboutLink);
 menuList.appendChild(about);
 
-let contact = document.createElement("li");
-contact.className = "contatc-content";
-let contactLink = document.createElement("a");
-contactLink.textContent = "Contact";
-contactLink.className = "mobile-a";
-contactLink.href = "#contact-section";
+const contact = document.createElement('li');
+contact.className = 'contatc-content';
+const contactLink = document.createElement('a');
+contactLink.textContent = 'Contact';
+contactLink.className = 'mobile-a';
+contactLink.href = '#contact-section';
 contact.appendChild(contactLink);
 menuList.appendChild(contact);
 
-let bars = document.getElementById("bars");
-bars.addEventListener("click", () => {
-  menu.style.display = "block";
+const bars = document.getElementById('bars');
+bars.addEventListener('click', () => {
+  menu.style.display = 'block';
 });
 
-let close = document.getElementById("x-close");
-close.addEventListener("click", () => {
-  menu.style.display = "none";
+const close = document.getElementById('x-close');
+close.addEventListener('click', () => {
+  menu.style.display = 'none';
 });
 
-document.getElementsByClassName("mobile-a")[0].addEventListener("click", () => {
-  menu.style.display = "none";
+document.getElementsByClassName('mobile-a')[0].addEventListener('click', () => {
+  menu.style.display = 'none';
 });
 
-document.getElementsByClassName("mobile-a")[1].addEventListener("click", () => {
-  menu.style.display = "none";
+document.getElementsByClassName('mobile-a')[1].addEventListener('click', () => {
+  menu.style.display = 'none';
 });
 
-document.getElementsByClassName("mobile-a")[2].addEventListener("click", () => {
-  menu.style.display = "none";
+document.getElementsByClassName('mobile-a')[2].addEventListener('click', () => {
+  menu.style.display = 'none';
 });
